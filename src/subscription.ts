535b5df6b8c81bb1d2a5afb351b83c40d1c9da83
sumijune <140953069+sumijune@users.noreply.github.com>
2023-07-30 14:31:26 +0200

    Update subscription.ts

--- src/subscription.ts
+++ src/subscription.ts
@@ -9,14 +9,7 @@ export class FirehoseSubscription extends FirehoseSubscriptionBase {
     if (!isCommit(evt)) return
     const ops = await getOpsByType(evt)
 
-    // This logs the text of every post off the firehose.
-    // Just for fun :)
-    // Delete before actually using
-    for (const post of ops.posts.creates) {
-      console.log(post.record.text)
-    }
-
-    const postsToDelete = ops.posts.deletes.map((del) => del.uri)
+       const postsToDelete = ops.posts.deletes.map((del) => del.uri)
     const postsToCreate = ops.posts.creates
       .filter((create) => {
         // only alf-related posts
